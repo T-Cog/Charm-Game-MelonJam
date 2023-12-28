174b2c8fe2fb308f5fb074b815e8e8c7c9ca0e4e
T-Cog <tcoghlan3@live.ca>
2023-12-28 16:21:48 -0500

    Dialogue in progress
    Mid work on dialogue system

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Image actorImage;
+    public Text actorName;
+    public Text messageText;
+    public RectTransform backgroundBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+
+    public void OpenDialogue(Message[] messages, Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+
+        Debug.Log("Started conversation. Loaded messages:" + messages.Length);
+        DisplayMessage();
+    }
+
+    void DisplayMessage()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
